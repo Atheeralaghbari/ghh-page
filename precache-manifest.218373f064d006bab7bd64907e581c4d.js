eefe3a08b2ebf7dd9dde50c4c32903f7a99f8af1
Atheeralaghbari <atheeralaghbari61@gmail.com>
2019-11-24 17:35:16 +0330

    Updates

---
+++ precache-manifest.218373f064d006bab7bd64907e581c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e301ac6eb257c430d06f918d997b07",
+    "url": "/ghh-page/index.html"
+  },
+  {
+    "revision": "3f6a9eaf2ccf99fa4357",
+    "url": "/ghh-page/static/css/main.0b7834a3.chunk.css"
+  },
+  {
+    "revision": "c782beb07b128c8c4511",
+    "url": "/ghh-page/static/js/2.82edb450.chunk.js"
+  },
+  {
+    "revision": "3f6a9eaf2ccf99fa4357",
+    "url": "/ghh-page/static/js/main.b2fdcbf1.chunk.js"
+  },
+  {
+    "revision": "ebe4de1d3ab87c5be5bd",
+    "url": "/ghh-page/static/js/runtime-main.c41d6a78.js"
+  }
+]);
